864c68330cccef82dabc263d2616cf81d944eb11
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-06-22 21:41:36 +0200

    Added OverPopulation rule

---
+++ src/GameOfLife/Rule/OverPopulationDeath.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GameOfLife\Rule;
+
+use GameOfLife\Cell;
+use GameOfLife\Action;
+
+/**
+ * GameOfLife\Rule\OverPopulationDeath
+ */
+class OverPopulationDeath extends AbstractRule
+{
+    public function apply(Cell $cell)
+    {
+        if ($this->getNumberOfLivingNeighbours($cell) > 3) {
+            return new Action\Kill($cell);
+        }
+
+        return new Action\Null($cell);
+    }
+}
