2518f08848e061ac4681eec5678bab39a1dc5511
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-06-25 22:53:21 +0200

    Added Methods to board

--- src/GameOfLife/Board.php
+++ src/GameOfLife/Board.php
@@ -2,10 +2,37 @@
 
 namespace GameOfLife;
 
+use GameOfLife\Board\Neighbourhood;
+
 /**
  * GameOfLife\Board
  */
 class Board
 {
+    /**
+     * @param Cell[]
+     */
+    private $cells = array();
 
+    /**
+     * @param array[Cell[]]
+     */
+    public function __construct(array $cells)
+    {
+        $neighbourhood = new Neighbourhood($cells);
+        $this->cells = $neighbourhood->getResidents();
+
+        foreach ($this->getCells() as $cell) {
+            $neighbours = $neighbourhood->getNeighbours($cell);
+            $cell->setNeighbours($neighbours);
+        }
+    }
+
+    /**
+     * @param Cell[]
+     */
+    public function getCells()
+    {
+        return $this->cells;
+    }
 }
--- tests/GameOfLifeTest/BoardTest.php
+++ tests/GameOfLifeTest/BoardTest.php
@@ -2,8 +2,9 @@
 
 namespace GameOfLifeTest;
 
-use PHPUnit_Framework_TestCase;
 use GameOfLife\Board;
+use GameOfLife\Cell;
+use PHPUnit_Framework_TestCase;
 
 /**
  * GameOfLifeTest\BoardTest
@@ -12,17 +13,71 @@ class BoardTest extends PHPUnit_Framework_TestCase
 {
     /** @var Board */
     protected $board;
+    /** @var Cell[] */
+    protected $cells;
 
     public function setUp()
     {
-        $this->board = new Board();
+        $this->cells = $this->getCells();
+        $this->board = new Board($this->cells);
+    }
+
+    private function getCells()
+    {
+        return array(
+            array(new Cell(), new Cell(), new Cell()),
+            array(new Cell(), new Cell(), new Cell()),
+            array(new Cell(), new Cell(), new Cell()),
+        );
     }
 
     /**
      * @test
      */
-    public function getCells()
+    public function setNeighboursCorrectly()
     {
-        $this->markTestIncomplete();
+        $cell = $this->cells[0][0];
+        $expected = array(
+            $this->cells[0][1],
+            $this->cells[1][0],
+            $this->cells[1][1],
+        );
+
+        $actual = $cell->getNeighbours();
+        $this->assertEquals($expected, $actual);
+
+        $cell = $this->cells[1][1];
+        $expected = array(
+            $this->cells[0][0],
+            $this->cells[0][1],
+            $this->cells[0][2],
+            $this->cells[1][0],
+            $this->cells[1][2],
+            $this->cells[2][0],
+            $this->cells[2][1],
+            $this->cells[2][2],
+        );
+
+        $actual = $cell->getNeighbours();
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanGetCells()
+    {
+        $cells = array(
+            $this->cells[0][0],
+            $this->cells[0][1],
+            $this->cells[0][2],
+            $this->cells[1][0],
+            $this->cells[1][1],
+            $this->cells[1][2],
+            $this->cells[2][0],
+            $this->cells[2][1],
+            $this->cells[2][2],
+        );
+        $this->assertEquals($cells, $this->board->getCells());
     }
 }
